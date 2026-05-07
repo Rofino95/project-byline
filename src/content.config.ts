db16ba38404b5a2a8840db0c426a0c680e6e48c7
RobinZ <rofinosgt@gmail.com>
2026-05-08 00:19:00 +0200

    Artikel

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,12 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
+import { glob } from 'astro/loaders';
 
 const articlesCollection = defineCollection({
+  loader: glob({
+    pattern: '**/*.md',
+    base: './src/content/articles',
+  }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
